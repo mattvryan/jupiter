a70ca82ed3c8e3be82f10e13054bb3bae20fbead
Matt Ryan <matt@mvryan.org>
2014-01-07 07:17:33 -0700

    [#1] Add stubs for configuration unit tests.

---
+++ apps/ios/Jupiter/JupiterTests/ConfigurationTests.m
@@ -0,0 +1,94 @@
+//
+//  ConfigurationTests.m
+//  Jupiter
+//
+//  Created by Matt Ryan on 1/7/14.
+//  Copyright (c) 2014 Seventeen Stones. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ConfigurationTests : SenTestCase
+
+@end
+
+@implementation ConfigurationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testInitCreatesEmptyConfigurationStore
+{
+    STFail(@"Not implemented");
+}
+
+- (void)testSetStringProperty
+{
+    STFail(@"Not implemented");
+}
+
+- (void)testSetIntegerProperty
+{
+    STFail(@"Not implemented");
+}
+
+- (void)testSetDoubleProperty
+{
+    STFail(@"Not implemented");
+}
+
+- (void)testSetBooleanProperty
+{
+    STFail(@"Not implemented");
+}
+
+- (void)testSetObjectProperty
+{
+    STFail(@"Not implemented");
+}
+
+- (void)testInitUsesExistingConfigurationStore
+{
+    STFail(@"Not implemented");
+}
+
+- (void)testInitRecreatesEmptyConfigurationStoreIfInvalid
+{
+    STFail(@"Not implemented");
+}
+
+- (void)testGetStringProperty
+{
+    STFail(@"Not implemented");
+}
+
+- (void)testGetIntegerProperty
+{
+    STFail(@"Not implemented");
+}
+
+- (void)testGetDoubleProperty
+{
+    STFail(@"Not implemented");
+}
+
+- (void)testGetBooleanProperty
+{
+    STFail(@"Not implemented");
+}
+
+- (void)testGetObjectProperty
+{
+    STFail(@"Not implemented");
+}
+
+@end
